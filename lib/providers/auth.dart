0e34b589d3862881467055a021ef47c93eb99028
aman arora <aman.arora2@globallogic.com>
2020-04-23 17:37:33 +0530

    api key saved

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -33,7 +33,7 @@ class Auth with ChangeNotifier {
   Future<void> _authenticate(
       String email, String password, String urlSegment) async {
     final url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyAtf2NYEoLgYpKJyDkCKS84nndTgFg2Xzs';
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=API_Key';
     try {
       final response = await http.post(
         url,
